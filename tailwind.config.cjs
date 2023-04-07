85e7d8f7fad015db40cb33db098cfac64ebaae93
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-04-07 11:12:59 +1000

    using the new build methods

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,25 @@ module.exports = {
   theme: {
     extend: {},
   },
+  dasisyui: {
+    themes : [
+      {
+        scoutstas: {
+          "primary": "#009059",
+          "secondary": "#005a37",
+          "accent": "#95cb6e",
+          "neutral": "#3d5c92",
+          "base-100": "#1a416f",
+          "info": "#008f88",
+          "success": "#79b143",
+          "warning": "#f3b819",
+          "error": "#ea594b"
+        },
+      },
+    ]
+  },
   plugins: [
     require('@tailwindcss/typography'),
-    require("daisyui")],
+    require("daisyui")
+  ]
 }
