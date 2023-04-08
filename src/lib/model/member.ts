4a74988cd3fb16946885bfba9a3ff035e58848eb
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-04-09 01:44:36 +1000

    no id

--- src/lib/model/member.ts
+++ src/lib/model/member.ts
@@ -2,9 +2,8 @@ import { memberType } from "./memberType";
 
 export class member {
     
-    public id: number = 0;
-    public registrationNumber: number = -1;
     public name: string = "";
+    public registrationNumber: number = -1;
     public dateOfBirth: Date = new Date("01-01-1900");
     public memberType: memberType = memberType.unknown;
     public phoneNumber: string = "";
