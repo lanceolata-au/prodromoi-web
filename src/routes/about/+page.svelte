d216be01dcdcdf479d4e3d04e34a730d4dea6af9
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-04-13 19:11:30 +1000

    center everything

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,27 +1,29 @@
-<div class="text-l text-neutral bg-neutral-content/90 p-1 rounded max-w-2xl m-2 mt-10 pb-2">
-    <h1 class="text-2xl text-center">About</h1>
-</div>
-<div class="text-l text-neutral bg-neutral-content/90 p-1 rounded max-w-2xl m-2 mt-5">
-    <h1 class="text-2xl text-center">License</h1>
-    <br>
-    Prodromoi - A toolbox of automated business rules for Scouts Tasmania <br>
-    Copyright (C) 2023 Owen Holloway (<a class="text-primary-focus" href="https://lanceolata.com.au">lanceolata.com.au</a>)
-    <br><br>
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    <br><br>
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    <br><br>
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see https://www.gnu.org/licenses/.
-    <br><br>
-    Souce fully avaliable: 
-    <br>
-    <a class="underline text-primary-focus" href="https://github.com/lanceolata-au/prodromoi-api">API Souces</a> & 
-    <a class="underline text-primary-focus" href="https://github.com/lanceolata-au/prodromoi-web">Web Souces</a>
+<div class="grid place-items-center">
+    <div class="text-l text-neutral bg-neutral-content/90 p-1 rounded max-w-2xl m-2 mt-10 pb-2">
+        <h1 class="text-2xl text-center">About</h1>
+    </div>
+    <div class="text-l text-neutral bg-neutral-content/90 p-1 rounded max-w-2xl m-2 mt-5">
+        <h1 class="text-2xl text-center">License</h1>
+        <br>
+        Prodromoi - A toolbox of automated business rules for Scouts Tasmania <br>
+        Copyright (C) 2023 Owen Holloway (<a class="text-primary-focus" href="https://lanceolata.com.au">lanceolata.com.au</a>)
+        <br><br>
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+        <br><br>
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+        <br><br>
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see https://www.gnu.org/licenses/.
+        <br><br>
+        Souce fully avaliable: 
+        <br>
+        <a class="underline text-primary-focus" href="https://github.com/lanceolata-au/prodromoi-api">API Souces</a> & 
+        <a class="underline text-primary-focus" href="https://github.com/lanceolata-au/prodromoi-web">Web Souces</a>
+    </div>
 </div>
